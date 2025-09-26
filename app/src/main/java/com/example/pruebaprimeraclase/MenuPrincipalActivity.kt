4d5e1c62ae1055daccb1d5b167830cda926ed55b
Kevin <kevin.delbello@bue.edu.ar>
2025-09-26 20:05:26 -0300

    Inicio

---
+++ app/src/main/java/com/example/pruebaprimeraclase/MenuPrincipalActivity.kt
@@ -0,0 +1,14 @@
+package com.example.pruebaprimeraclase
+
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MenuPrincipalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_principal)
+
+    }
+}
