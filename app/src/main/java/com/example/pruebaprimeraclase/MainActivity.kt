8304abd2a0b0ae0e5791202aa89871b0c4720f6b
Kevin <kevin.delbello@bue.edu.ar>
2025-10-02 20:00:21 -0300

    clase 02-10

--- app/src/main/java/com/example/pruebaprimeraclase/MainActivity.kt
+++ app/src/main/java/com/example/pruebaprimeraclase/MainActivity.kt
@@ -11,5 +11,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, MenuPrincipalActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
