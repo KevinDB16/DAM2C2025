059ac10dd94887134a1108e5b0a4845be4b2de14
Kevin <kevin.delbello@bue.edu.ar>
2025-10-13 20:27:52 -0300

    cambios clase 09-10

--- app/src/main/java/com/example/pruebaprimeraclase/MenuPrincipalActivity.kt
+++ app/src/main/java/com/example/pruebaprimeraclase/MenuPrincipalActivity.kt
@@ -1,14 +1,35 @@
 package com.example.pruebaprimeraclase
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 
 class MenuPrincipalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_principal)
+        val tvBienvenida = findViewById<TextView>(R.id.tvBienvenida)
+        val usuario = intent.getStringExtra("usuario") ?: "Usuario"
+        tvBienvenida.text = "Bienvenido $usuario"
 
+        Snackbar.make(findViewById(android.R.id.content), "Sesión iniciada...", Snackbar.LENGTH_LONG)
+            .show()
+
+        val btnSalir = findViewById<Button>(R.id.btnSalir)
+        btnSalir.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle("Cerrar sesión")
+                .setMessage("¿Desea cerrar la sesión?")
+                .setPositiveButton("Sí") {_,_ ->
+                    finish()
+                }
+                .setNegativeButton("No", null)
+                .show()
+        }
     }
 }
